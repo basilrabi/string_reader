2799c4aadcd1dd8dac27813ee9d47fb06860be40
Basil Eric Rabi <ericbasil.rabi@gmail.com>
2020-07-14 20:37:54 +0800

    use set to avoid duplication of point descriptions
    fixes #1

--- geom.py
+++ geom.py
@@ -35,7 +35,7 @@ class Segment:
     def __init__(self, points):
         self.str_id = None
         self.point_list = points
-        self.description_list = [None] * 100
+        self.description_list = [set() for i in range(0, 100)]
         for point in self.point_list:
             if not self.str_id:
                 self.str_id = point.str_id
@@ -43,10 +43,8 @@ class Segment:
                 if self.str_id != point.str_id:
                     raise Exception('String id of points do not match.')
             for i in range(0, len(point.descriptions)):
-                if self.description_list[i]:
-                    self.description_list[i] +=  f', {point.descriptions[i]}'
-                else:
-                    self.description_list[i] =  point.descriptions[i]
+                if point.descriptions[i]:
+                    self.description_list[i].add(point.descriptions[i])
 
     def asQgsFeature(self):
         feature = QgsFeature()
@@ -61,7 +59,7 @@ class Segment:
         return 'LINESTRING (' + ','.join(coordinates) + ')'
 
     def getAttributes(self):
-        return self.description_list[0:Point.description_count]
+        return [','.join(self.description_list[i]) or None for i in range(0, Point.description_count)]
 
     @staticmethod
     def qgsFields():
